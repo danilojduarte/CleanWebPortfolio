911771b14315a0f5dcc7b1e60c6408586521a1e0
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2024-02-23 15:22:44 -0300

    Efeitos concluidos

--- js/script.js
+++ js/script.js
@@ -25,3 +25,8 @@ const sr = ScrollReveal ({
 })
 
 sr.reveal('.home-text', {delay:350, origin:'left'})
+sr.reveal('.home-img', {delay:350, origin:'right'})
+
+
+
+sr.reveal('.sub-service, .about, .portfolio, .service, .cta, .contact, .footer', {delay:200, origin:'bottom'})
