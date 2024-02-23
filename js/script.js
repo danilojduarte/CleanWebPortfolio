47e65555bd70dd7a875a4d581ae10dd17a91ca86
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2024-02-23 14:14:27 -0300

    Aplicando javascript para esconder menu

--- js/script.js
+++ js/script.js
@@ -3,3 +3,16 @@ const header = document.querySelector("header");
 window.addEventListener ("scroll", function() {
     header.classList.toggle ("sticky", window.scrollY > 0);
 });
+
+let menu = document.querySelector('#menu-icon');
+let navList = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navList.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navList.classList.remove('active');
+}
